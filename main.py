9d01b5cb778daa717926f3b67caebcbe0f2accd6
Osama <osama.yousef@tuta.io>
2025-12-05 20:47:36 +0200

    feat: add the template main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+def main():
+    pass
+
+def add(n1, n2):
+    pass
+
+def sub(n1, n2):
+    pass
+
+def mul(n1, n2):
+    pass
+
+def div(n1, n2):
+    pass
+
+if __name__ == "__main__":
+    print(add(1, 5))
+    print(sub(1, 5))
+    print(div(1, 5))
+    print(mul(1, 5))
